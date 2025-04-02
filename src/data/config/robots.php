ebda7af9de084f90b6378ad4675ab3186a620500
quang10122003 <qdao10122003badao@gmail.com>
2025-04-02 10:39:24 +0700

    fix bug path

--- src/data/config/robots.php
+++ src/data/config/robots.php
@@ -14,4 +14,4 @@ if (!defined('NV_MAINFILE')) {
 }
 
 $cache = 'a:6:{s:6:"/data/";i:0;s:10:"/includes/";i:0;s:9:"/install/";i:0;s:9:"/modules/";i:0;s:11:"/robots.php";i:0;s:11:"/web.config";i:0;}';
-$cache_other = 'a:6:{s:18:"/vi/banners/click/";i:0;s:15:"/vi/statistics/";i:0;s:10:"/vi/users/";i:0;s:23:"/TTCN/vi/banners/click/";i:0;s:20:"/TTCN/vi/statistics/";i:0;s:15:"/TTCN/vi/users/";i:0;}';
+$cache_other = 'a:6:{s:18:"/vi/banners/click/";i:0;s:15:"/vi/statistics/";i:0;s:10:"/vi/users/";i:0;s:27:"/TTCN/src/vi/banners/click/";i:0;s:24:"/TTCN/src/vi/statistics/";i:0;s:19:"/TTCN/src/vi/users/";i:0;}';
